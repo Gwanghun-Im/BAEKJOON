0f37dad5351fe394b9ac4ee4a19753767ea2a69e
Gwanghun Im <lgh82229047@gmail.com>
2021-01-20 23:28:00 +0900

    bkjn 1010

--- 210120/1010.py
+++ 210120/1010.py
@@ -4,4 +4,13 @@ n = int(input())
 
 for i in range(n):
     a, b = map(int, input().split())
+    answer = 1
+
+    for i in range(a):
+        answer *= (b-i)
+
+    for i in range(1,a+1) :
+        answer //= i
     
+    print(answer)
+
