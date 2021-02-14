7744e4a6fbc7bfe20c19a55540e09a21bf191f39
임광훈 <lgh82229047@gmail.com>
2021-02-14 23:11:32 +0900

    6588 bkjn modified

--- 210214/6588.py
+++ 210214/6588.py
@@ -1,25 +1,26 @@
 # 6588번 : 골드바흐의 추측
 
-sieve = [1 for i in range(10000001)]
+import sys
+
+sieve = [1 for i in range(1000001)]
 sieve[1] = 0
-for i in range(2, 10000001):
+shorten = int(1000001**(0.5))+1
+
+for i in range(2, shorten):
     if sieve[i] == 0:
         continue
-    for j in range(2*i, 10000001, i):
+    for j in range(2*i, 1000001, i):
         sieve[j] = 0
 
 def solve(n):
-    for i in range(n):
-            if sieve[i] == 0:
-                continue
-            for j in range(i, n):
-                if sieve[j] == 0:
-                    continue
-                if i+j == n:
-                    return f'{n} = {i} + {j}'
-    return '"Goldbach\'s conjecture is wrong.\'"'
+    for i in range(3, n, 2):
+        if sieve[i] :
+            if sieve[n-i] == 1:
+                return f'{n} = {i} + {n-i}'
 
-n = int(input())
-while n != 0:
+    return "Goldbach\'s conjecture is wrong."
+
+while True:
+    n = int(sys.stdin.readline().strip())
+    if n == 0: break
     print(solve(n))
-    n = int(input())
