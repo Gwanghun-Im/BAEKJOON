0eb2d21c17dd256f09754fabfb606db8c881143f
임광훈 <lgh82229047@gmail.com>
2021-06-03 00:35:20 +0900

    5021 bkjn

--- 210602/5021.py
+++ 210602/5021.py
@@ -1,20 +1,30 @@
 # 5021번 : 왕위 계승
-from collections import Counter
+from collections import deque
 
 N, M = map(int, input().split())
 family_tree = {}
-family_tree[input()] = 1
+family_tree[input()] = 10000
 
-names = []
-candi = []
-parent = {}
+parent = []
 for _ in range(N):
-    c, p1, p2 = input().split()
-    parent[c] = [p1,p2]
-    names += [c, p1, p2]
+    parent.append(input().split())
+
+for _ in range(N):
+    for c, p1, p2 in parent:
+        if p1 not in family_tree.keys():
+            family_tree[p1] = 0
+        if p2 not in family_tree.keys():
+            family_tree[p2] = 0
+        family_tree[c] = (family_tree[p1]+family_tree[p2])/2
+
+name = ''
+blood = 0
 for _ in range(M):
     temp = input()
-    names += [temp]
-    candi += [temp]
-
-print(Counter(names))
+    try:
+        if blood<family_tree[temp]:
+            name = temp
+            blood = family_tree[temp]
+    except:
+        continue
+print(name)
