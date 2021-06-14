1a5795b3d8b5ab8c4fed792d61a6072bebb883f5
임광훈 <lgh82229047@gmail.com>
2021-06-14 22:01:24 +0900

    2206 bkjn

--- 210604/2206.py
+++ 210604/2206.py
@@ -10,33 +10,27 @@ dx = [0, 1, 0, -1]
 N, M = map(int, input().split())
 my_map = [list(map(int, list(input().rstrip()))) for _ in range(N)]
 dis = [[float('inf')]*M for _ in range(N)]
+
 dis[0][0]=0
 
-chance = False
 h = []
-heapq.heappush(h, (1 ,0, 0))
+heapq.heappush(h, (0, 1 ,0, 0))
 while h:
-    w, y, x = heapq.heappop(h)
+    wall, w, y, x = heapq.heappop(h)
     for d in range(4):
-        use = False
         my = y + dy[d]
         mx = x + dx[d]
         if 0<=my<N and 0<=mx<M and dis[my][mx] > w+1:
             if my_map[my][mx]==0:
                 dis[my][mx] = w + 1
-                heapq.heappush(h, (w+1, my, mx))
-            elif not chance:
-                use = True
-                chance = True
+                heapq.heappush(h, (wall, w+1, my, mx))
+            if my_map[my][mx]==1 and not wall:
                 dis[my][mx] = w + 1
-                heapq.heappush(h, (w+1, my, mx))
-        if use:
-            chance = True
+                heapq.heappush(h, (1, w+1, my, mx))
 
-if dis[-1][-1] == float('inf'):
+if dis[-1][-1]==float('inf'):
     print(-1)
+elif not dis[-1][-1]:
+    print(1)
 else:
     print(dis[-1][-1])
-
-for i in dis:
-    print(*i)
