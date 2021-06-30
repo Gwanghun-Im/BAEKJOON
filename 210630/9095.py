a0d3dee38445e8fc216f0fea017df93e10a0d9af
Gwanghun-Im <lgh82229047@gmail.com>
2021-06-30 21:45:20 +0900

    9095 bkjn

---
+++ 210630/9095.py
@@ -0,0 +1,9 @@
+# 9095번: 1, 2, 3 더하기
+dp = [0] * 11
+dp[1] = 1
+dp[2] = 2
+dp[3] = 4
+for i in range(4, 11):
+    dp[i] = dp[i-1] + dp[i-2] + dp[i-3]
+for i in range(int(input())):
+    print(dp[int(input())])
